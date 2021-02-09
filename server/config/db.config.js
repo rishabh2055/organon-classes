00345c31e6788df5e73da02673de50246f8ddcd0
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2021-02-09 20:21:43 +0530

    Changed credentials

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,7 +1,7 @@
 export const DB = {
   HOST: '85.187.128.41',
-  USER: 'organonc_USER',
-  PASSWORD: 'ORg^#$gjhhj3453',
+  USER: 'organonc',
+  PASSWORD: 'k!mk)IlEI976I6',
   DB: 'organonc_DB',
   dialect: 'mysql',
   pool: { // pool is optional, it will be used for Sequelize connection pool configuration
